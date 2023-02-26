f29cc797ea9e8e17be3e84c08d062702954bfb5f
Yaeno <ing.zhang@gmail.com>
2023-02-26 09:38:12 -0800

    01 Class Note Code

---
+++ 01_introductionToC.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    printf("比特\n");
+    printf("%d\n",sizeof(char));
+    printf("%d\n",sizeof(short));
+    printf("%d\n",sizeof(int));
+    printf("%d\n",sizeof(long));
+    printf("%d\n",sizeof(long long));
+    printf("%d\n",sizeof(float));
+    printf("%d\n",sizeof(double));
+    printf("%d\n",sizeof(long double));
+
+    return 0;
+}
